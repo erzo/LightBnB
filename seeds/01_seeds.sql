f028b80e9913875e1dac15aecfef4bc900ccb4af
Felipe Gonzalez <felipefagnilli@gmail.com>
2020-07-22 23:59:00 +0000

    modified

--- seeds/01_seeds.sql
+++ seeds/01_seeds.sql
@@ -3,7 +3,7 @@ VALUES ('Sue Luna', 'sue.luna@gmx.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBI
 ('Etta West', 'etta.west@gmx.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
 ('Leroy Hart', 'leroy.hart@gmx.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
 
-INSERT INTO properties(owner_id, title, description, thumbnail_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street,city,province, post_code, active)
+INSERT INTO properties(owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city , province, post_code, active)
 VALUES (1, 'the tardis', 'description', 'https://images.pixels.com/1', 'https://images.pixels.com/1-tiny', 940.75, 6, 4, 8, 'Canada', '651 Nami Code', 'Bohbatev', 'Alberta', '83680', true),
 (2, 'the corner store', 'description', 'https://images.pixels.com/2', 'https://images.pixels.com/2-tiny', 1000.10, 1, 2, 4, 'Canada', '123 ABC Street', 'White Rock', 'BC', '83717', true),
 (3, 'your moms house', 'description', 'https://images.pixels.com/3', 'https://images.pixels.com/3-tiny', 4200.42, 3, 5, 6, 'Canada', '727 Hero Road', 'Burnaby', 'BC', '82345', true);
