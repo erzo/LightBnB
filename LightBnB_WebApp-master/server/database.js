53b87349045de086f3c64828e3dcd70f8f86e0f1
Felipe Gonzalez <felipefagnilli@gmail.com>
2020-07-24 02:07:56 +0000

    modified property listings to create a new property listing

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -90,6 +90,7 @@ const addUser =  function(user) {
   RETURNING *;
   `, [user.name, user.email, user.password])
   .then(res => {
+    console.log(res.rows[0]);
     return res.rows[0]
   })
   .catch(error => {
@@ -143,21 +144,6 @@ exports.getAllReservations = getAllReservations;
  * @param {*} limit The number of results to return.
  * @return {Promise<[{}]>}  A promise to the properties.
  */
-// const getAllProperties = function(options, limit = 10) {
-//   const limitedProperties = {};
-//   for (let i = 1; i <= limit; i++) {
-//     limitedProperties[i] = properties[i];
-//   }
-//   return Promise.resolve(limitedProperties);
-// }
-
-// const getAllProperties = function(options, limit = 10) {
-  // return pool.query(`
-  // SELECT * FROM properties
-  // LIMIT $1
-  // `, [limit])
-  // .then(res => res.rows);
-// }
 
 const getAllProperties = function(options, limit = 10) {
   // 1
@@ -224,9 +210,51 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  
+  return pool.query(`
+  INSERT INTO properties (owner_id,
+    title,
+    description,
+    thumbnail_photo_url,
+    cover_photo_url,
+    cost_per_night,
+    street,
+    city,
+    province,
+    post_code,
+    country,
+    parking_spaces,
+    number_of_bathrooms,
+    number_of_bedrooms)
+  VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
+  RETURNING *;
+  `, [property.owner_id,
+    property.title,
+    property.description,
+    property.thumbnail_photo_url,
+    property.cover_photo_url,
+    property.cost_per_night,
+    property.street,
+    property.city,
+    property.province,
+    property.post_code,
+    property.country,
+    property.parking_spaces,
+    property.number_of_bathrooms,
+    property.number_of_bedrooms])
+  .then(res => {
+    console.log(res.rows[0]);
+    return res.rows[0]
+  })
+  .catch(error => {
+    return null
+  })
 }
+
 exports.addProperty = addProperty;
+
+
+// const propertyId = Object.keys(properties).length + 1;
+  // property.id = propertyId;
+  // properties[propertyId] = property;
+  // return Promise.resolve(property);
